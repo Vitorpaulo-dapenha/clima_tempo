44e7c1412270cf893ec4ea34ec968616766488b1
Vitor Paulo da Penha <vitorpaulodapenha03@gmail.com>
2024-09-20 21:07:45 -0300

    Update: Criação do projeto clima tempo

---
+++ README.md
@@ -0,0 +1,32 @@
+# Projeto Clima Tempo
+Este projeto é uma aplicação web simples que exibe informações sobre o clima de uma cidade escolhida. O aplicativo permite ao usuário pesquisar por uma cidade e obter detalhes sobre o clima atual, como temperatura, umidade e condições meteorológicas, usando a API de clima.
+
+## Funcionalidades
+Pesquisa de cidade para exibir o clima atual
+Exibição de temperatura, umidade e descrição do clima
+Design responsivo para diferentes tamanhos de tela
+
+### Tecnologias Utilizadas
+HTML5: Estruturação do conteúdo da página.
+CSS3: Estilização da página com foco em responsividade.
+JavaScript: Lógica para fazer requisições à API e manipulação do DOM.
+API de Clima: Integração com uma API para obter os dados meteorológicos.
+
+#### Como Executar o Projeto
+
+1 - Faça o download ou clone o repositório: git clone https://github.com/vitor-penha/clima_tempo.git
+
+2 - Navegue até o diretório do projeto: cd clima-tempo
+
+3 - Abra o arquivo index.html no navegador de sua preferência.
+
+##### Estrutura do Projeto
+
+index.html: Contém a estrutura da interface do usuário.
+
+style.css: Arquivo de estilos que define o design e a responsividade da aplicação.
+
+script.js: Contém toda a lógica de busca da cidade e a exibição dos dados obtidos da API de clima.
+
+icons/: Diretório com ícones utilizados na aplicação, como ícones de clima.
+
---
+++ script.js
@@ -0,0 +1,106 @@
+const searchInput = document.querySelector(".search-input");
+const locationButton = document.querySelector(".location-button");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const hourlyWeather = document.querySelector(".hourly-weather .weather-list");
+
+const API_KEY = "d10ebc536cc34a91b63180014242009";
+
+
+const weatherCodes = {
+  clear: [1000],
+  clouds: [1003, 1006, 1009],
+  mist: [1030, 1135, 1147],
+  rain: [1063, 1150, 1153, 1168, 1171, 1180, 1183, 1198, 1201, 1240, 1243, 1246, 1273, 1276],
+  moderate_heavy_rain: [1186, 1189, 1192, 1195, 1243, 1246],
+  snow: [1066, 1069, 1072, 1114, 1117, 1204, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264, 1279, 1282],
+  thunder: [1087, 1279, 1282],
+  thunder_rain: [1273, 1276],
+}
+
+
+const displayHourlyForecast = (hourlyData) => {
+  const currentHour = new Date().setMinutes(0, 0, 0);
+  const next24Hours = currentHour + 24 * 60 * 60 * 1000;
+
+
+  const next24HoursData = hourlyData.filter(({ time }) => {
+    const forecastTime = new Date(time).getTime();
+    return forecastTime >= currentHour && forecastTime <= next24Hours;
+  });
+
+
+  hourlyWeather.innerHTML = next24HoursData.map((item) => {
+    const temperature = Math.floor(item.temp_c);
+    const time = item.time.split(' ')[1].substring(0, 5);
+    const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(item.condition.code));
+
+    return `<li class="weather-item">
+            <p class="time">${time}</p>
+            <img src="icons/${weatherIcon}.svg" class="weather-icon">
+            <p class="temperature">${temperature}°</p>
+          </li>`;
+  }).join('');
+};
+
+
+const getWeatherDetails = async (API_URL) => {
+  window.innerWidth <= 768 && searchInput.blur();
+  document.body.classList.remove("show-no-results");
+
+  try {
+
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+
+    const temperature = Math.floor(data.current.temp_c);
+    const description = data.current.condition.text;
+    const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code));
+
+
+    currentWeatherDiv.querySelector(".weather-icon").src = `icons/${weatherIcon}.svg`;
+    currentWeatherDiv.querySelector(".temperature").innerHTML = `${temperature}<span>°C</span>`;
+    currentWeatherDiv.querySelector(".description").innerText = description;
+
+
+    const combinedHourlyData = [...data.forecast?.forecastday[0]?.hour, ...data.forecast?.forecastday[1]?.hour];
+
+    searchInput.value = data.location.name;
+    displayHourlyForecast(combinedHourlyData);
+  } catch (error) {
+    document.body.classList.add("show-no-results");
+  }
+}
+
+
+const setupWeatherRequest = (cityName) => {
+  const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
+  getWeatherDetails(API_URL);
+}
+
+
+searchInput.addEventListener("keyup", (e) => {
+  const cityName = searchInput.value.trim();
+
+  if (e.key == "Enter" && cityName) {
+    setupWeatherRequest(cityName);
+  }
+});
+
+
+locationButton.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+      getWeatherDetails(API_URL);
+      window.innerWidth >= 768 && searchInput.focus();
+    },
+    () => {
+      alert("Location access denied. Please enable permissions to use this feature.");
+    }
+  );
+});
+
+
+setupWeatherRequest("London");
